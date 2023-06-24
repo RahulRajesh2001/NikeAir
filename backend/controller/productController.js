1e0d16e8f593a8d9a7b3a6d07d65b8cc3834ecd4
RahulRajesh2001 <rahulrajesh2001239@gmail.com>
2023-06-24 23:03:12 +0530

    structured the backend route folders

---
+++ backend/controller/productController.js
@@ -0,0 +1,19 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/nikeShoesModel.js"
+
+
+//fetch all products
+export const getProducts =asyncHandler(async(req,res)=>{
+    const products=await Product.find({});
+    res.json(products);
+})
+
+//fetch a single product
+export const getProductById =asyncHandler(async(req,res)=>{
+    const product =await Product.findById(req.params.id);
+    if(product){
+      res.json(product)
+    }else{
+       res.status(404).json({message:'Product not found'})
+    }  
+})
