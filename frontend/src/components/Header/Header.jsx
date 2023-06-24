0a6891dde6f6e5ad0b5c1282ae6c39020be32331
RahulRajesh2001 <rahulrajesh2001239@gmail.com>
2023-06-24 13:17:07 +0530

    react router dom is implimented

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,32 +1,47 @@
-import React from 'react';
-import {Navbar,Nav,Container} from 'react-bootstrap';
-import '../Header/header.css';
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import '../Header/header.css'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <header>
- <Navbar expand="lg" collapseOnSelect className="navbar-white">
-  <Container>
-    <Navbar.Brand href="/">NIKE</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mx-auto">
-        <Nav.Link href="/cart">MAN</Nav.Link>
-        <Nav.Link href="/cart">WOMEN</Nav.Link>
-        <Nav.Link href="/cart">KIDS</Nav.Link>
-        <Nav.Link href="/cart">COLLECTIONS</Nav.Link>
-        <Nav.Link href="/cart">CONTACT</Nav.Link>
-      </Nav>
-      <Nav className="ms-auto">
-        <Nav.Link href="/cart">SEARCH</Nav.Link>
-        <Nav.Link href="/login">CART</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+      <Navbar expand='lg' collapseOnSelect className='navbar-white'>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>NIKE</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='mx-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>MAN</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/'>
+                <Nav.Link>WOMEN</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/'>
+                <Nav.Link>KIDS</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/'>
+                <Nav.Link>COLLECTIONS</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/'>
+                <Nav.Link>CONTACT</Nav.Link>
+              </LinkContainer>
+            </Nav>
 
-
-
+            <Nav className='ms-auto'>
+              <LinkContainer to='/'>
+                <Nav.Link>SEARCH</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/'>
+                <Nav.Link>CART</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   )
 }
--- frontend/src/components/Product/Product.jsx
+++ frontend/src/components/Product/Product.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
-import {Card} from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 border-0'>
-        <a href={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant='top'/>
-        </a>
-        <Card.Body>
-        <a href={`/product/${product._id}`}>
-            <Card.Title as='div'>
-                <strong>{product.name}</strong>
-            </Card.Title>
-        </a>
-        <Card.Text as='h3'>
-            ₹{product.price}
-        </Card.Text>
-        </Card.Body>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+        <Card.Text as='h3'>₹{product.price}</Card.Text>
+      </Card.Body>
     </Card>
   )
 }
