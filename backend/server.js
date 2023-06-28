5e939442098fec059b9c0491fd1b6940a9dab00f
RahulRajesh2001 <rahulrajesh2001239@gmail.com>
2023-06-28 20:30:32 +0530

    deployement changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 dotenv.config();
 import cookieParser from "cookie-parser";
 import connectDB from './config/db.js';
@@ -12,6 +13,7 @@ const port =process.env.PORT || 5000;
 connectDB();// connect mongoDB
 const app=express();
 
+
 //body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
@@ -19,14 +21,26 @@ app.use(express.urlencoded({extended:true}))
 //Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/',(req,res)=>{
-    res.send("APi is running...")
-})
+
 
 app.use('/api/products',productRoutes); 
 app.use('/api/users',userRoutes);
 
-
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+  } else {
+    const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
 
 
 app.listen(port,()=> console.log(`Server running on port ${port}`))
